4427e5b7c9a0eb83cccef1209f116d48fb33ae61
unknown <ksemenza13@gmail.com>
2020-03-31 20:30:19 -0400

    update

---
+++ build/precache-manifest.5ec6f0df24ac2fa7fc0053b243a3c587.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dbbb2345f09f38532d4bea065f10bb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39f3c1c71cdeaffb7c42",
+    "url": "/static/css/main.9ad2e55b.chunk.css"
+  },
+  {
+    "revision": "a9ac026c5b908c7c3674",
+    "url": "/static/js/2.ac9905df.chunk.js"
+  },
+  {
+    "revision": "39f3c1c71cdeaffb7c42",
+    "url": "/static/js/main.215358f1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
